f27a51dcfc71edf3936783541d94b09363a70c66
andruhus <54791980+andruhus@users.noreply.github.com>
2026-05-06 13:11:16 +0300

    Initial commit

---
+++ bart_generation.py
@@ -0,0 +1,156 @@
+import argparse
+import random
+
+import numpy as np
+import pandas as pd
+import torch
+from sacrebleu.metrics import BLEU
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
+from transformers import AutoTokenizer, BartForConditionalGeneration
+
+from optimizer import AdamW
+
+
+TQDM_DISABLE = False
+
+
+def transform_data(dataset, max_length=256):
+    """
+    Turn the data to the format you want to use.
+    Use AutoTokenizer to obtain encoding (input_ids and attention_mask).
+    Tokenize the sentence pair in the following format:
+    sentence_1 + SEP + sentence_1 segment location + SEP + paraphrase_type_ids.
+    Return Data Loader.
+    """
+    ### TODO 
+    raise NotImplementedError
+
+
+def train_model(model, train_data, dev_data, device, tokenizer):
+    """
+    Train the model. Return and save the model.
+    """
+    ### TODO
+    raise NotImplementedError
+
+
+def test_model(test_data, test_ids, device, model, tokenizer):
+    """
+    Test the model. Generate paraphrases for the given sentences (sentence1) and return the results
+    in form of a Pandas dataframe with the columns 'id' and 'Generated_sentence2'.
+    The data format in the columns should be the same as in the train dataset.
+    Return this dataframe.
+    """
+    ### TODO
+    raise NotImplementedError
+
+
+def evaluate_model(model, test_data, device, tokenizer):
+    """
+    You can use your train/validation set to evaluate models performance with the BLEU score.
+    test_data is a Pandas Dataframe, the column "sentence1" contains all input sentence and 
+    the column "sentence2" contains all target sentences
+    """
+    model.eval()
+    bleu = BLEU()
+    predictions = []
+
+    dataloader = transform_data(test_data, shuffle=False)
+    with torch.no_grad():
+        for batch in dataloader: 
+            input_ids, attention_mask, _ = batch
+            input_ids = input_ids.to(device)
+            attention_mask = attention_mask.to(device)
+
+            # Generate paraphrases
+            outputs = model.generate(
+                input_ids,
+                attention_mask=attention_mask,
+                max_length=50,
+                num_beams=5,
+                early_stopping=True,
+            )
+            
+            pred_text = [
+                tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+                for g in outputs
+            ]
+            
+            predictions.extend(pred_text)
+
+    inputs = test_data["sentence1"].tolist()
+    references = test_data["sentence2"].tolist()
+
+    model.train()
+    # Calculate BLEU score
+    bleu_score_reference = bleu.corpus_score(references, [predictions]).score
+    # Penalize BLEU score if its to close to the input
+    bleu_score_inputs = 100 - bleu.corpus_score(inputs, [predictions]).score
+
+    print(f"BLEU Score: {bleu_score_reference}", f"Negative BLEU Score with input: {bleu_score_inputs}")
+    
+
+    # Penalize BLEU and rescale it to 0-100
+    # If you perfectly predict all the targets, you should get an penalized BLEU score of around 52
+    penalized_bleu = bleu_score_reference * bleu_score_inputs / 52
+    print(f"Penalized BLEU Score: {penalized_bleu}")
+
+    return penalized_bleu
+
+
+def seed_everything(seed=11711):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=11711)
+    parser.add_argument("--use_gpu", action="store_true")
+    args = parser.parse_args()
+    return args
+
+
+def finetune_paraphrase_generation(args):
+    device = torch.device("cuda") if args.use_gpu else torch.device("cpu")
+    model = BartForConditionalGeneration.from_pretrained("facebook/bart-large", local_files_only=True)
+    model.to(device)
+    tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large", local_files_only=True)
+
+    train_dataset = pd.read_csv("data/etpc-paraphrase-train.csv")
+    test_dataset = pd.read_csv("data/etpc-paraphrase-generation-test-student.csv")
+    dev_dataset = pd.read_csv("data/etpc-paraphrase-dev.csv")
+
+    # You might do a split of the train data into train/validation set here
+    # ...
+
+    train_data = transform_data(train_dataset)
+    dev_data = transform_data(dev_dataset)
+    test_data = transform_data(test_dataset)
+
+    print(f"Loaded {len(train_dataset)} training samples.")
+
+    model = train_model(model, train_data, dev_dataset, device, tokenizer)
+
+    print("Training finished.")
+
+    bleu_score = evaluate_model(model, dev_dataset, device, tokenizer)
+    print(f"The penalized BLEU-score of the model is: {bleu_score:.3f}")
+
+    test_ids = test_dataset["id"]
+    test_results = test_model(test_data, test_ids, device, model, tokenizer)
+    test_results.to_csv(
+        "predictions/bart/etpc-paraphrase-generation-test-output.csv", index=False
+    )
+
+
+if __name__ == "__main__":
+    args = get_args()
+    seed_everything(args.seed)
+    finetune_paraphrase_generation(args)
