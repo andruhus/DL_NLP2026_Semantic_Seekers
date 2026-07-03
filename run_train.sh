fa30a9d8e0408102069b293e10e00a8273a0c87d
Andrii Demydenko <aaademed@gmail.com>
2026-07-03 14:55:32 +0000

    Updated run_train for sst

--- run_train.sh
+++ run_train.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 #SBATCH --job-name=train-multitask_classifier
 #SBATCH -t 00:20:00                  # estimated time # TODO: adapt to your needs
 #SBATCH -p grete:shared              # the partition you are training on (i.e., which nodes), for nodes see sinfo -p grete:shared --format=%N,%G
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1                   # total number of tasks
 #SBATCH --cpus-per-task=8            # number cores per task
 #SBATCH --mail-type=all              # send mail when job begins and ends
-#SBATCH --mail-user=TODO@stud.uni-goettingen.de   
+SBATCH --mail-user=andrii.demydenko@stud.uni-goettingen.de   
 #SBATCH --output=./slurm_files/slurm-%x-%j.out     # where to write output, %x give job name, %j names job id
 #SBATCH --error=./slurm_files/slurm-%x-%j.err      # where to write slurm error
 
@@ -31,4 +31,4 @@ echo "Latest Commit: $(git rev-parse --short HEAD)"
 echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 
 # Run the script:
-python -u multitask_classifier.py --use_gpu --local_files_only --option finetune --task sst --hidden_dropout_prob 0.1
+python -u multitask_classifier.py --use_gpu --local_files_only --option pretrain --task sst --hidden_dropout_prob 0.1
