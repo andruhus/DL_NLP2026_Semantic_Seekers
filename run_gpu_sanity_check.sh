70e8cb5982e45f8df58cc8ef69ff789d3fca2f5e
SimonP <simon.pummer@stud.uni-goettingen.de>
2026-07-23 08:26:33 +0200

    run_gpu

--- run_gpu_sanity_check.sh
+++ run_gpu_sanity_check.sh
@@ -1,35 +1,35 @@
-#!/bin/bash
-#SBATCH --job-name=gpu_sanity_check
-#SBATCH -t 00:00:10                  # estimated time # TODO: adapt to your needs
-#SBATCH -p grete:shared              # the partition you are training on (i.e., which nodes), for nodes see sinfo -p grete:shared --format=%N,%G
-#SBATCH -G A100:1                    # take 1 GPU, see https://docs.hpc.gwdg.de/how_to_use/compute_partitions/gpu_partitions/index.html for more options
-#SBATCH --mem-per-gpu=8G             # setting the right constraints for the splitted gpu partitions
-#SBATCH --nodes=1                    # total number of nodes
-#SBATCH --ntasks=1                   # total number of tasks
-#SBATCH --cpus-per-task=8            # number cores per task
-#SBATCH --mail-type=all              # send mail when job begins and ends
-#SBATCH --mail-user=TODO@stud.uni-goettingen.de   
-#SBATCH --output=./slurm_files/slurm-%x-%j.out     # where to write output, %x give job name, %j names job id
-#SBATCH --error=./slurm_files/slurm-%x-%j.err      # where to write slurm error
+  #!/bin/bash
+  #SBATCH --job-name=gpu_sanity_check
+  #SBATCH -t 00:00:10                  # estimated time # TODO: adapt to your needs
+  #SBATCH -p grete:shared              # the partition you are training on (i.e., which nodes), for nodes see sinfo -p grete:shared --format=%N,%G
+  #SBATCH -G A100:1                    # take 1 GPU, see https://docs.hpc.gwdg.de/how_to_use/compute_partitions/gpu_partitions/index.html for more options
+  #SBATCH --mem-per-gpu=8G             # setting the right constraints for the splitted gpu partitions
+  #SBATCH --nodes=1                    # total number of nodes
+  #SBATCH --ntasks=1                   # total number of tasks
+  #SBATCH --cpus-per-task=8            # number cores per task
+  #SBATCH --mail-type=all              # send mail when job begins and ends
+  #SBATCH --mail-user=simon.pummer@stud.uni-goettingen.de
+  #SBATCH --output=./slurm_files/slurm-%x-%j.out     # where to write output, %x give job name, %j names job id
+  #SBATCH --error=./slurm_files/slurm-%x-%j.err      # where to write slurm error
 
-source activate dnlp # Or whatever you called your environment.
+  source activate dnlp # Or whatever you called your environment.
 
-# Printing out some info.
-echo "Submitting job with sbatch from directory: ${SLURM_SUBMIT_DIR}"
-echo "Home directory: ${HOME}"
-echo "Working directory: $PWD"
-echo "Current node: ${SLURM_NODELIST}"
+  # Printing out some info.
+  echo "Submitting job with sbatch from directory: ${SLURM_SUBMIT_DIR}"
+  echo "Home directory: ${HOME}"
+  echo "Working directory: $PWD"
+  echo "Current node: ${SLURM_NODELIST}"
 
-# For debugging purposes.
-python --version
-python -m torch.utils.collect_env 2> /dev/null
+  # For debugging purposes.
+  python --version
+  python -m torch.utils.collect_env 2> /dev/null
 
-# Print out some git info.
-module load git
-echo -e "\nCurrent Branch: $(git rev-parse --abbrev-ref HEAD)"
-echo "Latest Commit: $(git rev-parse --short HEAD)"
-echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
+  # Print out some git info.
+  module load git
+  echo -e "\nCurrent Branch: $(git rev-parse --abbrev-ref HEAD)"
+  echo "Latest Commit: $(git rev-parse --short HEAD)"
+  echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 
-# Run the script:
-python -c "import torch;  num_of_gpus = torch.cuda.device_count(); print('Number of GPU:',num_of_gpus);"
+  # Run the script:
+  python -c "import torch;  num_of_gpus = torch.cuda.device_count(); print('Number of GPU:',num_of_gpus);"
 
