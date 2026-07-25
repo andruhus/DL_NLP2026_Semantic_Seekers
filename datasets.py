c93183a76fc98d37c82a7c595377dcfaae1dd937
SimonP <simon.pummer@stud.uni-goettingen.de>
2026-07-25 12:52:28 +0200

    ceased->cased

--- datasets.py
+++ datasets.py
@@ -32,7 +32,7 @@ class SentenceClassificationDataset(Dataset):
         self.dataset = dataset
         self.p = args
         self.tokenizer = BertTokenizer.from_pretrained(
-            "bert-base-unceased", local_files_only=args.local_files_only
+            "bert-base-uncased", local_files_only=args.local_files_only
         )
 
     def __len__(self):
