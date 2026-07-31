2b5f37b7236a1c0718fa1bf456d60f8ffd34a7a6
SimonP <simon.pummer@stud.uni-goettingen.de>
2026-07-31 13:04:33 +0200

    correction

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -116,7 +116,7 @@ class MultitaskBERT(nn.Module):
         Returns a combined embedding: [CLS ; mean ; max]
         """
         outputs = self.bert(input_ids, attention_mask)
-        last_hidden = outputs.last_hidden_state  # (batch, seq_len, hidden)
+        last_hidden = outputs["last_hidden_state"]  # (batch, seq_len, hidden)
 
         cls = last_hidden[:, 0]  # CLS token
 
