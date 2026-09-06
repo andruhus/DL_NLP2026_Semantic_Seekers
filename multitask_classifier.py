9fb7ee4f2aeae4d0bd83395771cecd34bbe1d19a
SimonP <simon.pummer@stud.uni-goettingen.de>
2026-09-06 21:57:10 +0200

    0warmup

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -629,7 +629,7 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    args.filepath = f"models/warmup0.2{args.option}-{args.epochs}-{args.lr}-{args.task}.pt"  # save path
+    args.filepath = f"models/warmup0{args.option}-{args.epochs}-{args.lr}-{args.task}.pt"  # save path
     # Logging aktivieren
     logfile = setup_logging(args.filepath)
     seed_everything(args.seed)  # fix the seed for reproducibility
