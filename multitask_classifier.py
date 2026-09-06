376dcdb3f3b789379d0657d1581fed4d6815e94e
SimonP <simon.pummer@stud.uni-goettingen.de>
2026-09-06 14:47:16 +0200

    label

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -629,10 +629,9 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    args.filepath = f"models/testpretrainingyelp{args.option}-{args.epochs}-{args.lr}-{args.task}.pt"  # save path
+    args.filepath = f"models/labelsmooting0.01{args.option}-{args.epochs}-{args.lr}-{args.task}.pt"  # save path
     # Logging aktivieren
     logfile = setup_logging(args.filepath)
     seed_everything(args.seed)  # fix the seed for reproducibility
     train_multitask(args)
     test_model(args)
-
--- run_train.sh
+++ run_train.sh
@@ -31,4 +31,4 @@ echo "Latest Commit: $(git rev-parse --short HEAD)"
 echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 
 # Run the script:
-python -u multitask_classifier.py --use_gpu --local_files_only --option finetune --task sst --hidden_dropout_prob 0.1
+python -u multitask_classifier.py --task sst --option finetune --lr 2e-05 --batch_size 16 --warmup_ratio 0.1 --weight_decay 0 --label_smoothing 0.01 --classifier_dropout 0.3 --use_gpu --local_files_only > output.log 2> error.log
