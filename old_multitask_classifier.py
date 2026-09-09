9562f7ae2d665475856ca068de3800457334e3d8
SimonP <simon.pummer@stud.uni-goettingen.de>
2026-09-09 21:49:33 +0200

    old file for predictions

--- old_multitask_classifier.py
+++ old_multitask_classifier.py
@@ -634,3 +634,4 @@ if __name__ == "__main__":
     logfile = setup_logging(args.filepath)
     seed_everything(args.seed)  # fix the seed for reproducibility
     train_multitask(args)
+    test_model(args)
